f4c0e8b60c648eabe66544147aeceb898bf1861d
Nguyễn Thanh Duy <nguyenthanhduy220507@gmail.com>
2024-03-13 16:37:40 +0700

    first commit

---
+++ src/main/java/com/coyote/StudentAttendanceApp/StudentAttendanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.coyote.StudentAttendanceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAttendanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAttendanceAppApplication.class, args);
+	}
+
+}
